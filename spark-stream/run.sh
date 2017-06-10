a683317a6f09f9882969cb37aca7340f91e977da
cosmos342 <cosmos342@github.com>
2017-06-10 00:28:11 -0700

    running spark streaming job

---
+++ spark-stream/run.sh
@@ -0,0 +1,1 @@
+/usr/local/spark/bin/spark-submit --master  spark://ec2-34-208-213-63.us-west-2.compute.amazonaws.com:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.1 ./get_kafka_stream.py
